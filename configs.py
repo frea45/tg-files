80c52939160572a74511a146bc9170cc6901e762
frea45 <110092585+frea45@users.noreply.github.com>
2022-09-08 16:12:39 +0430

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,13 +4,13 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	API_ID = int(os.environ.get("API_ID", "3335796"))
+	API_HASH = os.environ.get("API_HASH","138b992a0e672e8346d8439c3f42ea78")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN","")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME","ir_uploadbot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "763990585"))
+	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://abirhasan2005:abirhasan@cluster0.lb2tp.mongodb.net/cluster0?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
